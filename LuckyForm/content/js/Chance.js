3bf1afbe8c363df263d6dcde95c7f52fc6dd04a9
nir shalom <nirshalom111@gmail.com>
2019-03-11 09:18:51 +0200

    chance

---
+++ LuckyForm/content/js/Chance.js
@@ -0,0 +1,31 @@
+﻿$("document").ready(
+    function () {
+        $("img.card_type_img").click(
+            function () {
+                var limit = 1;
+                if ($(this).parent().hasClass("strong_numbers")) {
+                    limit = 1;
+                }
+                if (formType === 3) {
+                    limit = 17;
+                }
+
+                if (countChosenNumbers($(this).parent()) >= limit && !$(this).hasClass("clicked")) {
+                    //error border
+
+                }
+                else {
+                    if ($(this).hasClass("clicked")) {
+                        $(this).removeClass("clicked");
+                        $(this).children().attr('checked', false)
+                    }
+                    else {
+                        $(this).addClass("clicked");
+                        $(this).children().attr('checked', true)
+                    }
+                }
+
+                tableValidition($(this));
+            });
+
+    });
