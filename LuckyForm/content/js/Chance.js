6c99c00282878bf8520870b8b5c330bf72d3b274
nir shalom <nirshalom111@gmail.com>
2019-03-13 14:00:33 +0200

    chance js

--- LuckyForm/content/js/Chance.js
+++ LuckyForm/content/js/Chance.js
@@ -6,9 +6,9 @@ $("document").ready(
         $(".card_img").click(
             function () {
 
-                var limit = 6;
-                if (formID == 10) {
-                    limit = 1;
+                var limit = 1;
+                if (formID == 12) {
+                    limit = 4;
                 }
                 
 
@@ -19,15 +19,43 @@ $("document").ready(
                 else {
                     if ($(this).hasClass("clicked")) {
                         $(this).removeClass("clicked");
+                        $(this).siblings(".card_img").removeClass("not_clicked");
                         $(this).siblings("input").attr('checked', false)
                     }
                     else {
                         $(this).addClass("clicked");
-                        $(this).siblings("input").attr('checked', true)
+                        $(this).siblings(".card_img").addClass("not_clicked");
+                        $(this).prev("input").attr('checked', true)
                     }
                 }
 
                 tableValidition($(this));
+            });
+        $(".submit_tables").click(
+            function () {
+                var form = $(this).parents('form:first');
+                if (formType === 10) {
+                    if (!formValidation(form)) {
+                        alert("not valid");
+                        return;
+                    }
+                    else if (!(perfectTableCount(form) > 0 && perfectTableCount(form) % 2 == 0)) {
+                        alert("not pairs");
+                        return;
+                    }
+                    form.submit();
+                }
+                else {
+                    if (!formValidation(form)) {
+                        alert("not valid");
+                        return;
+                    }
+                    else if (!perfectTableCount(form) > 0) {
+                        alert("zero");
+                        return;
+                    }
+                    form.submit();
+                }
             });
 
     });
@@ -44,22 +72,32 @@ function countChosenNumbers(element)
     return count;
 }
 function tableValidition(element) {
-
+    element.parents('.card_type').addClass("table_error");
     var reqNums = 1;
-    if (formType === 10) {
-        reqNums = 1;
-    }
-
-    if (countChosenNumbers(element.parents('.card_type')) < reqNums && countChosenNumbers(element.parents('.card_type')) > 0) {
-        element.parents('.card_type').addClass("table_error");
-    }
-    else if (countChosenNumbers(element.parents('.card_type')) == 0) {
+    
+    if (countChosenNumbers(element.parents('.card_type')) == 0) {
         element.parents('.card_type').removeClass("table_error");
         element.parents('.card_type').removeClass("table_perfect");
+        return false;
 
     }
     else {
         element.parents('.card_type').removeClass("table_error");
         element.parents('.card_type').addClass("table_perfect");
+        return true;
     }
+}
+
+function formValidation(form) {
+
+    var isValid = true;
+    if(formI)
+    form.find('.card_type').each(function () {
+        if (!tableValidition($(this))) {
+            isValid = false;
+            $(this).addClass("table_error");
+        }
+
+    });
+    return isValid;
 }
