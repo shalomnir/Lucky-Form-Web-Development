eebc9e585ed35ef91c202985fdfb064b04abc972
nir shalom <nirshalom111@gmail.com>
2019-03-27 14:00:39 +0200

    web-service

---
+++ CreditCompany/IService1.cs
@@ -0,0 +1,48 @@
+﻿using CreditCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CreditCompany
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetDealVerification(CreditCard card, double amount, int payments);
+
+        [OperationContract]
+        string GetDealsReport(DateTime start, DateTime end);
+        
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ CreditCompany/Models/CreditCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CreditCompany.Models
+{
+    public class CreditCard
+    {
+        public string Number { get; set; }
+        public DateTime ExpirationDate  { get; set; }
+        public string cvv { get; set; }
+    }
+}
